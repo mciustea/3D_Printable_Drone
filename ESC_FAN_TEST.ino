5753853ff3010d8ad92d446c16c2011896d6bb43
mciustea <mciustea@uw.edu>
2019-03-21 00:11:02 -0700

    First EDF Fan Control Test with Serial Communication & Servo Control

--- ESC_FAN_TEST.ino
+++ ESC_FAN_TEST.ino
@@ -1,11 +1,30 @@
 #include <Servo.h>
 
 
-int MOTOR_PIN = 9;
+int ESC_PIN1 = 7;
+int ESC_PIN2 = 5;
+int LEFT_AILERON = 2;
+int RIGHT_AILERON = 4;
+int FRONT_WHEEL = 9;
+int ELEVATOR = 10;
+int RUDDER = 11;
+
 int MAX_SIGNAL = 2000;
 int MIN_SIGNAL = 700;
-Servo motor;
+Servo ESC2;
+Servo ESC1;
 int delayT = 0;
+int X = 0;
+int Zw = 0;
+int Y = 0;
+int Z = 0;
+int T = 0;
+
+Servo frontWheel;
+Servo aileronL;
+Servo aileronR;
+Servo Elevator;
+Servo Rudder;
 
 const char HEADER       = 'H';
 const char A_TAG    = 'M';
@@ -18,8 +37,14 @@ void setup() {
   
   Serial.begin(9600);
   Serial1.begin(57600); //radio
-  motor.attach(MOTOR_PIN);
-  motor.attach(MOTOR_PIN);
+  frontWheel.attach(FRONT_WHEEL);
+  ESC1.attach(ESC_PIN1);
+  ESC2.attach(ESC_PIN2);
+  aileronL.attach(LEFT_AILERON);
+  aileronR.attach(RIGHT_AILERON);
+  Elevator.attach(ELEVATOR);
+  Rudder.attach(RUDDER);
+  //ESC1.attach(ESC_PIN1);
   
   Serial.println("Boot up the ESC? 1 for Yes, 2 for No");
   while(!Serial.available()){
@@ -30,11 +55,13 @@ void setup() {
     Serial.println("Booting up ESC !");
     Serial.println("Writing max");
     Serial.println("2s till start ");
-    motor.writeMicroseconds(MAX_SIGNAL);
+    ESC1.writeMicroseconds(MAX_SIGNAL);
+    ESC2.writeMicroseconds(MAX_SIGNAL);
     delay(2000);
     Serial.println("Writing min");
     Serial.println("ESC Booted");
-    motor.writeMicroseconds(MIN_SIGNAL);
+    ESC1.writeMicroseconds(MIN_SIGNAL);
+    ESC2.writeMicroseconds(MIN_SIGNAL);
   } else {
     Serial.println("No ESC Booted");
   }
@@ -51,11 +78,17 @@ void loop() {
   
   if(Serial1.peek() != -1){
     readSerial();
-    if((thrust >= 700) && (thrust <= 2000)){
+    if((thrust >= 700) && (thrust <= 2000) && (X != 0) &&(Y != 0) && (Z != 0)){
       //String output = "Thrust is " + String(thrust);
       //Serial.print("Thrust is ");
       //Serial.println(thrust);
-      motor.writeMicroseconds(thrust);
+      ESC1.writeMicroseconds(thrust);
+      ESC2.writeMicroseconds(thrust);
+      aileronL.write(X);
+      aileronR.write(X);
+      Elevator.write(Y);
+      Rudder.write(Z);
+      frontWheel.write(Zw);
     }  
   }
   //delay(10);
@@ -75,19 +108,23 @@ if ( Serial1.available() >= TOTAL_BYTES)
          
         int a = Serial1.read() * 256; 
         a = a + Serial1.read();
+        X = a;
         delay(delayT);  
         int b = Serial1.read() * 256;
         delay(delayT); 
         b = b + Serial1.read();
+        Y = b;
         delay(delayT);  
         int c = Serial1.read() * 256;
         delay(delayT); 
         c = c + Serial1.read();
+        Z = c;
         delay(delayT);  
         int d = Serial1.read() * 256;
         delay(delayT); 
         d = d + Serial1.read();
         thrust = d; 
+        Zw = map(Z, 20, 160, 140, 40);
         /*
         String rxStr = "Received integers | a:" + String(a) +
            ", b:" + String(b) + ", c:" + String(c) + ", d:" + 
